1954295cd37db37e6422b317f15bc1bf35e53fc8
saanvi-ranadive <sranadive@ucsd.edu>
2025-04-18 22:14:36 -0700

    added global.js and updated features in JS

---
+++ global.js
@@ -0,0 +1,62 @@
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"
+  : "/portfolio/";
+
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+    return Array.from(context.querySelectorAll(selector));
+  }
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact'},
+    { url: 'https://github.com/saanvi-ranadive', title: 'Github'},
+    { url: 'resume/', title: 'Resume'}
+  ];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  // next step: create link and add it to nav
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
+  }
+  if (a.host !== location.host) {
+    a.target = "_blank";
+  }
+  nav.append(a);
+}
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+  <label class="color-scheme">
+    Theme:
+    <select>
+      <option value="auto">Auto</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>`,
+);
+
+const select = document.querySelector('select');
+
+if ("colorScheme" in localStorage) {
+  document.documentElement.style.setProperty('color-scheme', localStorage.colorScheme);
+  select.value = localStorage.colorScheme;
+}
+select.addEventListener('input', function (event) {
+  console.log('color scheme changed to', event.target.value);
+  document.documentElement.style.setProperty('color-scheme', event.target.value);
+  localStorage.colorScheme = event.target.value;
+});
