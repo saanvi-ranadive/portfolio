6833dd8f14c588c70d08433a79afca36d5a2886c
saanvi-ranadive <sranadive@ucsd.edu>
2025-05-02 10:54:29 -0700

    adjusted image paths

--- global.js
+++ global.js
@@ -80,9 +80,6 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
   containerElement.innerHTML = '';
 
   projects.forEach(project => {
-    const imagePath = project.image.startsWith('http') 
-      ? project.image 
-      : BASE_PATH + project.image.replace(/^\.\//, '').replace(/^\.\.\//, '');
       
     const article = document.createElement('article');
     article.innerHTML = `
