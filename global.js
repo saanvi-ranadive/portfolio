e3bc14719a0b4c1ff5fbb9e37bed8e3d42fcd273
saanvi-ranadive <sranadive@ucsd.edu>
2025-05-02 03:05:18 -0700

    adjust relative paths for project images using js

--- global.js
+++ global.js
@@ -80,6 +80,10 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
   containerElement.innerHTML = '';
 
   projects.forEach(project => {
+    const imagePath = project.image.startsWith('http') 
+      ? project.image 
+      : BASE_PATH + project.image.replace(/^\.\//, '').replace(/^\.\.\//, '');
+      
     const article = document.createElement('article');
     article.innerHTML = `
       <div class="project-header">
